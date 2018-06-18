83ca4fdd51f71d733e9141611181cc5a91c471d1
Fabián León <fadarsaleeing@gmail.com>
2018-06-18 18:47:17 -0500

    modificando info

--- start.sh
+++ start.sh
@@ -25,7 +25,4 @@ SCHEMASPY_SCHEMA=$SCHEMASPY_SCHEMA DB_BUILD_USER=$DB_BUILD_USER DB_BUILD_NAME=$D
 #docker-compose run schemaspy sh -c 'java -jar schemaSpy.jar -t pgsql -o /output -dp postgresql-jre6.jar -host database -port 5432 -u crud_user -p password -db udistrital_core_db -s financiera' 
 echo "ejecutando contenedor..."
 echo "El contenedor se esta ejecutando en segundo plano..."
-echo "para ver el log de los servicios utilice los siguientes comandos:"
-echo "para el cliente: CLIENT_PORT=$CLIENT_PORT CRUD_PORT=$CRUD_PORT MID_PORT=$MID_PORT docker-compose logs -f client"
-echo "para el mid api: CLIENT_PORT=$CLIENT_PORT CRUD_PORT=$CRUD_PORT MID_PORT=$MID_PORT docker-compose logs -f mid"
-echo "para el crud api: CLIENT_PORT=$CLIENT_PORT CRUD_PORT=$CRUD_PORT MID_PORT=$MID_PORT docker-compose logs -f crud"
+echo "para ver el log de los servicios utilice DockStation:"
